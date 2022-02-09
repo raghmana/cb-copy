b6d8ed146c8f38ecc9f3b2cf45e05044b05cfbf2
abd <raghman.gem@gmail.com>
2022-02-09 13:48:22 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -33,6 +33,35 @@ function updateTotalCost() {
     let fee = document.getElementById("deliveryFee");
     let totalPrice = document.getElementById("totalCost");
     cost.innerText = totalCost.toFixed(2);
-    fee.innerText = (totalCost*0.15).toFixed(2);
-    totalPrice.innerText = (totalCost + (totalCost*0.15)).toFixed(2);
+    fee.innerText = (totalCost * 0.15).toFixed(2);
+    totalPrice.innerText = (totalCost + (totalCost * 0.15)).toFixed(2);
 }
+
+function showPop1() {
+    document.getElementsByClassName('pop-1')[0].style.visibility = 'visible';
+}
+function hidePop1() {
+    document.getElementsByClassName('pop-1')[0].style.visibility = 'hidden';
+}
+function showPop2() {
+    document.getElementsByClassName('pop-3')[0].style.visibility = 'visible';
+}
+function hidePop2() {
+    document.getElementsByClassName('pop-3')[0].style.visibility = 'hidden';
+}
+function showPop3() {
+    document.getElementsByClassName('pop-2')[0].style.visibility = 'visible';
+}
+function hidePop3() {
+    document.getElementsByClassName('pop-2')[0].style.visibility = 'hidden';
+}
+
+function closeItem1() {
+    document.getElementsByClassName('item-1')[0].style.display='none';
+};
+function closeItem2() {
+    document.getElementsByClassName('item-2')[0].style.display='none';
+};
+function closeItem3() {
+    document.getElementsByClassName('item-3')[0].style.display='none';
+};
